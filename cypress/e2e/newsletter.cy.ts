8da909215d74499570f0acdb6baf8bb06302dd17
nelsoncode019 <nelsonher019@gmail.com>
2022-08-16 15:08:21 -0600

    type random id

--- cypress/e2e/newsletter.cy.ts
+++ cypress/e2e/newsletter.cy.ts
@@ -1,7 +1,9 @@
 describe("empty spec", () => {
   it("passes", () => {
     cy.visit("/");
-    cy.get("#email").type("nelson@gmail.com");
+    const uuid = () => Cypress._.random(0, 1e6);
+    const id = uuid();
+    cy.get("#email").type(`${id}@gmail.com`);
     cy.get("form").submit();
   });
 });
