4aa78d7664cccdd6bf9bbaeb606e56310473069d
nelsoncode019 <nelsonher019@gmail.com>
2022-08-17 11:26:18 -0600

    [ADD] Intercept fetch with cypress

--- cypress/e2e/newsletter.cy.ts
+++ cypress/e2e/newsletter.cy.ts
@@ -1,8 +1,12 @@
 describe("empty spec", () => {
   it("passes", () => {
     cy.visit("/");
+    cy.intercept("GET", "/api/event/get", { events: [] });
     const uuid = () => Cypress._.random(0, 1e6);
     const id = uuid();
+    cy.intercept("POST", "/api/email/suscribe", {
+      message: "success",
+    });
     cy.get("#email").type(`${id}@gmail.com`);
     cy.get("form").submit();
   });
