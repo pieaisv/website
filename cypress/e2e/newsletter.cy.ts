91e1d6f71c5acac79a9540c6dd3193c17f2640aa
nelsoncode019 <nelsonher019@gmail.com>
2022-08-19 22:33:43 -0600

    [FIX] problems with test

--- cypress/e2e/newsletter.cy.ts
+++ cypress/e2e/newsletter.cy.ts
@@ -2,12 +2,10 @@ describe("empty spec", () => {
   it("passes", () => {
     cy.visit("/");
     cy.intercept("GET", "/api/event/get", { events: [] });
-    const uuid = () => Cypress._.random(0, 1e6);
-    const id = uuid();
     cy.intercept("POST", "/api/email/suscribe", {
       message: "success",
     });
-    cy.get("#email").type(`${id}@gmail.com`);
+    cy.get("#email").type(`${crypto.randomUUID()}@gmail.com`);
     cy.get("form").submit();
   });
 });
