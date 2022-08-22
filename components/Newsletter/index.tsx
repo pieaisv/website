284a9f3357670a06793d08cb0069ca7d35d86bdd
nelsoncode019 <nelsonher019@gmail.com>
2022-08-21 21:09:45 -0600

    [FIX] Card newsletter for mobile

--- components/Newsletter/index.tsx
+++ components/Newsletter/index.tsx
@@ -19,7 +19,7 @@ function Newsletter() {
   return (
     <div className="m-3 md:m-auto max-w-screen-xl">
       <div className="module-border-wrap">
-        <div className="module">
+        <div className="module-newsletter">
           <div className="grid grid-cols-1 md:grid-cols-2">
             <div>
               <h2 className="text-3xl font-semibold mb-2">
@@ -27,11 +27,11 @@ function Newsletter() {
               </h2>
               <p className="text-base">
                 A newsletter to inform you about events and new updates from Pie
-                & AI San Salvador.
+                & AI: San Salvador.
               </p>
             </div>
             <form method="post" onSubmit={handleSubmit}>
-              <div className="grid-newsletter bg-white drop-shadow h-16 md:h-20 mt-4">
+              <div className="grid-newsletter bg-white rounded-md drop-shadow h-16 md:h-20 mt-4">
                 <div className="flex justify-center items-center">
                   <input
                     className="placeholder:text-black appearance-none border border-transparent w-full py-4 md:ml-6 px-2 h-16 text-black leading-tight focus:outline-none focus:shadow-outline bg-transparent"
@@ -61,7 +61,7 @@ function Newsletter() {
                       xmlns="http://www.w3.org/2000/svg"
                       className="h-7"
                       viewBox="0 0 20 20"
-                      fill="#ffb100"
+                      fill="#ffb100" 
                     >
                       <path
                         fillRule="evenodd"
