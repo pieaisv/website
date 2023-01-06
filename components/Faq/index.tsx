a1b4682a935a4b5875305e3bfc6383b67160b285
nelsoncode019 <nelsonher019@gmail.com>
2023-01-05 22:06:28 -0600

    [ADD] FAQ tag for SEO

--- components/Faq/index.tsx
+++ components/Faq/index.tsx
@@ -1,6 +1,11 @@
-import React from "react";
-
-function Faq() {
+interface FAQProps {
+  faqProps: Array<{
+    id: number;
+    title: string;
+    description: string;
+  }>;
+}
+function Faq({ faqProps }: FAQProps) {
   return (
     <div className="p-4">
       <br />
@@ -15,74 +20,18 @@ function Faq() {
       <br />
       <div>
         <div className="text-xl flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-          <div className="w-full lg:w-1/2 px-4 py-2">
-            <details className="mb-4">
-              <summary className="font-semibold cursor-pointer  bg-gray-200 rounded-md py-2 px-4 mb-3">
-                What is a Pie & AI?
-              </summary>
-              <span>
-                Pie & AI is a series from{" "}
-                <a
-                  href="https://www.deeplearning.ai/ambassador/"
-                  className="text-indigo-700 hover:underline cursor-pointer"
-                >
-                  DeepLearning.AI
-                </a>
-                . AI meetings hosted independently by the{" "}
-                <a
-                  href="https://www.deeplearning.ai/events/"
-                  className="text-indigo-700 hover:underline cursor-pointer"
-                >
-                  global AI community
-                </a>
-                . Events typically include conversations with world leaders,
-                thought-provoking discussions, networking opportunities with
-                your fellow learners, hands-on project practice, and cakes (or
-                other desserts of your choice).
-              </span>
-            </details>
-            <details className="mb-4">
-              <summary className="font-semibold cursor-pointer bg-gray-200 rounded-md py-2 px-4 mb-3">
-                How can I be an ambassador?
-              </summary>
-              <span>
-                The requirements are that you like to teach or organize AI
-                events, click here to{" "}
-                <a
-                  href="https://www.deeplearning.ai/ambassador/"
-                  className="text-indigo-700 hover:underline cursor-pointer"
-                >
-                  apply now
-                </a>
-              </span>
-            </details>
-          </div>
-          <div className="w-full lg:w-1/2 px-4 py-2">
-            <details className="mb-4">
-              <summary className="font-semibold cursor-pointer bg-gray-200 rounded-md py-2 px-4 mb-3">
-                What is San Salvador?
-              </summary>
-              <span>
-                San Salvador is the capital of the country El Salvador, the Pie
-                & AI are events in every city in the world
-              </span>
-            </details>
-            <details className="mb-4">
-              <summary className="font-semibold cursor-pointer bg-gray-200 rounded-md py-2 px-4 mb-3">
-                How can I give a talk?
-              </summary>
-              <span>
-                You can send us an email{" "}
-                <a
-                  href="mailto:pieaisv@gmail.com"
-                  className="text-indigo-700 hover:underline cursor-pointer"
-                >
-                  pieaisv@gmail.com{" "}
-                </a>{" "}
-                or you can also write to the ambassadors personally on Twitter
-              </span>
-            </details>
-          </div>
+          {faqProps.map(({ id, title, description }) => {
+            return (
+              <div key={id} className="w-full lg:w-1/2 px-4 py-2">
+                <details className="mb-4">
+                  <summary className="font-semibold cursor-pointer bg-gray-200 rounded-md py-2 px-4 mb-3">
+                    {title}
+                  </summary>
+                  <div dangerouslySetInnerHTML={{ __html: description }} />
+                </details>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Newsletter from "../components/Newsletter";
 import Presentation from "../components/Presentation";
 import ShareMeet from "../components/ShareMeet";
 import Sponsors from "../components/Sponsors";
+import Script from "next/script";
 
 const Home: NextPage = () => {
   // @ts-ignore
@@ -18,6 +19,23 @@ const Home: NextPage = () => {
   const description =
     "AI meetings hosted independently by DeepLearning.AI Event Ambassadors";
 
+  const parseLdJson = () => {
+    const data = {
+      "@context": "https://schema.org",
+      "@type": "FAQPage",
+      mainEntity: faq.map(({ title, description }) => {
+        return {
+          "@type": "Question",
+          name: title,
+          acceptedAnswer: {
+            "@type": "Answer",
+            text: description,
+          },
+        };
+      }),
+    };
+    return data;
+  };
   return (
     <>
       <Head>
@@ -35,14 +53,81 @@ const Home: NextPage = () => {
         <meta name="twitter:creator" content="@pieaisv" />
         <meta name="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(parseLdJson()) }}
+        />
       </Head>
       <Presentation />
       <Sponsors />
       <ShareMeet />
       <Ambassadors />
-      <Faq />
+      <Faq faqProps={faq} />
       <Newsletter />
     </>
   );
 };
 export default Home;
+
+const faq = [
+  {
+    id: 1,
+    title: "What is a Pie & AI?",
+    description: `
+      <p>
+        Pie & AI is a series from
+        <a
+          href="https://www.deeplearning.ai/ambassador/"
+          class="text-indigo-700 hover:underline cursor-pointer"
+        >
+          DeepLearning.AI
+        </a>.<strong>
+          AI meetings hosted independently by the global AI community.
+        </strong> Events typically include conversations with world leaders,
+        thought-provoking discussions, networking opportunities with your fellow
+        learners, hands-on project practice, and cakes (or other desserts of
+        your choice).
+      </p>
+    `,
+  },
+  {
+    id: 2,
+    title: "How can I be an ambassador?",
+    description: `
+      <p>
+        The requirements are that you like to teach or organize AI events, click
+        here to 
+        <a
+          href="https://www.deeplearning.ai/ambassador/"
+          class="text-indigo-700 hover:underline cursor-pointer"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          apply now
+        </a>
+        .
+      </p>
+    `,
+  },
+  {
+    id: 3,
+    title: "What is San Salvador?",
+    description: `<p>
+        San Salvador is the capital of the country El Salvador, the Pie & AI are
+        events in every city in the world.
+      </p>
+      `,
+  },
+  {
+    id: 4,
+    title: "How can I give a talk?",
+    description: `
+      <p>
+        You can <strong>send a private message on Twitter or LinkedIn</strong>
+        or you can write to the ambassadors and we will gladly schedule an event
+        with you
+      </p>
+    `,
+  },
+];
